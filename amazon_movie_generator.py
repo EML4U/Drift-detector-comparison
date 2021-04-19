1cf36ede7a13a123f5b5ca8e44faa19032ad1f9a
Robert Feldhans <rfeldhans@techfak.uni-bielefeld.de>
2021-04-19 10:07:04 +0200

    amazon movie embedding script now downloads, embeds, sorts the dataset, then deletes all unneccesary files

--- amazon_movie_generator.py
+++ amazon_movie_generator.py
@@ -5,6 +5,16 @@ import pickle
 from datetime import datetime
 import time
 
+## First: Download the dataset
+os.system('mkdir -p data/movies/embeddings')
+os.system('wget -c https://snap.stanford.edu/data/movies.txt.gz')
+os.system('gzip -d movies.txt.gz')
+os.system('mv movies.txt data/movies/movies.txt')
+
+
+
+## Second: embed everything, save in chunks so the memory doesn't explode
+
 bert = BertHuggingface(8, batch_size=8)
 
 counter = 0
@@ -51,3 +61,24 @@ with open('data/movies/movies.txt', 'r', errors='ignore') as f:
     text_list = []
     key_list = []
     print('At step', counter, 'of', 7_911_684/batch_size)
+
+# Third: reload all the files into memory (should work for machines with more than 32Gb RAM, confirmed to work on 64Gb)
+# and sort the reviews by time. Necessary because they are sorted by product code by default.
+os.system('rm data/movies/movies.txt') # movies.txt no longer needed
+
+embs = []
+key = []
+for counter in range(80):
+    with open('data/movies/embeddings/{}.pickle'.format(counter), 'rb') as handle:
+        data, keys = pickle.load(handle)
+    embs.extend(data)
+    key.extend(keys)
+
+# in-place sorting magic
+embs, key = (list(t) for t in zip(*sorted(zip(embs, key), key=lambda x: x[1][-1])))
+
+with open('data/movies/embeddings/amazon_ordered_by_time{}.pickle'.format(''), 'wb') as handle:
+    pickle.dump((embs, key), handle)
+    
+for i in range(counter + 1): # part files no longer needed
+    os.system('data/movies/embeddings/{}.pickle'.format(i))
