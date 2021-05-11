49a6f9c49ce72a73e53c57fdb26d268c15bafdc4
Adrian Wilke <adibaba@users.noreply.github.com>
2021-05-11 12:18:10 +0200

    Alibi Kolmogorov-Smirnov

--- detectors/AlibiKSDetector.py
+++ detectors/AlibiKSDetector.py
@@ -5,19 +5,23 @@ import numpy as np
 # Detector applies feature-wise two-sample Kolmogorov-Smirnov (K-S) tests
 # https://docs.seldon.io/projects/alibi-detect/en/stable/methods/ksdrift.html
 # https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test
-class AlibiKSDriftDetector(DriftDetector):
+class AlibiKSDetector(DriftDetector):
     
-    # p-value used for significance of the K-S test.
+    # p_val: p-value used for significance of the K-S test. 
+    #  If the FDR correction method is used, this corresponds to the acceptable q-value.
+    # correction: Correction type for multivariate data. Either 'bonferroni' or 'fdr' (False Discovery Rate).
     def __init__(self,
-                 p_val = 0.05):
+                 p_val = 0.05,
+                 correction = 'bonferroni'):
         self.p_val = p_val
+        self.correction = correction
         super().__init__(classifier=None)
         
     def fit(self, data, targets) -> DriftDetector:
         return self.fit(data)
     
     def fit(self, data) -> DriftDetector:
-        self.cd = KSDrift(data, p_val=self.p_val)
+        self.cd = KSDrift(data, p_val=self.p_val, correction=self.correction)
         return self
     
     def predict(self, data) -> bool:
