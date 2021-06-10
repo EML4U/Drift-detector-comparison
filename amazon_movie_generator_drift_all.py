6ae9118b2dbf89085c959cbe0568851209f0f60c
Adrian Wilke <adibaba@users.noreply.github.com>
2021-06-10 20:53:56 +0200

    generator: fixed key position

--- amazon_movie_generator_drift_all.py
+++ amazon_movie_generator_drift_all.py
@@ -7,8 +7,16 @@ import time
 import random
 random.seed(42)
 import re
+import yaml
 
-# target percentages for 
+# "bert_768" or "bow_50"
+mode = "bert_768"
+
+config          = yaml.safe_load(open("config.yaml", 'r'))
+amazon_raw_file = os.path.join(config["AMAZON_MOVIE_REVIEWS_DIRECTORY"], "amazon_raw.pickle")
+embeddings_file = os.path.join(config["EMBEDDINGS_DIRECTORY"], "amazon_drift_" + mode + ".pickle")
+
+# target percentages
 target_percentages = [0.005, 0.01, 0.02, 0.04, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 2.0, 4.0, 10.0, 20.0, 50.0, 100.0]
 num_samples = 500
 
@@ -16,23 +24,14 @@ num_samples = 500
 indices = []
 more_than_one = 0
 
-mode = "bert-768"
 
-## First: Download the dataset
-#os.system('mkdir -p data/movies/embeddings')
-#os.system('wget -c https://snap.stanford.edu/data/movies.txt.gz')
-#os.system('gzip -d movies.txt.gz')
-#os.system('mv movies.txt data/movies/movies.txt')
+## Embed everything, save in chunks so the memory doesn't explode
 
-
-
-## Second: embed everything, save in chunks so the memory doesn't explode
-
-# Configure model by mode
-if(mode == "bert-768"):
+# Configure model to use by mode
+if(mode == "bert_768"):
     bert = BertHuggingface(8, batch_size=8)
     embed = bert.embed
-elif(mode == "bow-50"):
+elif(mode == "bow_50"):
     print("bow")
     # TODO Required: funtion(text-list) -> list of embeddings
     # https://github.com/UBI-AGML-NLP/Embeddings/blob/main/embedding/doc2vec.py 
@@ -42,40 +41,30 @@ elif(mode == "bow-50"):
     #next(iter(amazon_reviews_reader))
     #embed = bow.infer_vector(next.iter tokens)
 else:
-    print("error")
-    exit()
+    raise ValueError("Unknown mode " + mode)
 
-with open('data/movies/embeddings/amazon_raw.pickle', 'rb') as handle:
+with open(amazon_raw_file, 'rb') as handle:
     texts, keys = pickle.load(handle)
 for i in range(len(keys)):
     keys[i][1] -= 1   # fix class names from 1..5 to 0..4 for easier 1-hot encoding
-    
-    
-original_data = []
-drift_data = []
-classes = [[] for x in range(5)]
-data = [x for x in list(zip(texts, keys)) if keys[0][-1] + timedelta(days=365*4) < x[1][-1] < keys[0][-1] + timedelta(days=365*(4+1))] # gather amazon reviews of the fourth year only
 
+# gather amazon reviews of the fourth year only
+classes = [[] for x in range(5)]
+data = [x for x in list(zip(texts, keys)) if keys[0][-2] + timedelta(days=365*4) < x[1][-2] < keys[0][-2] + timedelta(days=365*(4+1))]
 for point in data:
     classes[point[1][1]].append(point)
 for i in range(len(classes)):
     random.shuffle(classes[i])
 
+original_data = []
+drift_data = []
 for i in range(len(classes)):
     original_data.extend(classes[i][:num_samples])
     drift_data.extend(classes[i][num_samples:2*num_samples])
-        
-
-# add words            
-#negative_words = ['bad', 'horrible']
+       
+# Inject words            
 negative_words = ['waste', 'unwatchable', 'stinks', 'atrocious', 'yawn', 'ugh', 'abomination', 'garbage', 'worst', 'rubbish', 'defective', 'incoherent', 'ripoff', 'unconvincing', 'awful', 'dud', 'wasted', 'abysmal', 'travesty', 'wasting', 'poorly']
 
-#with open('data/sentiment_words/negative-words.txt', 'r', errors='ignore') as f:
-#    for line in f:
-#        if line and line.strip() and not line.startswith(';'):
-#            negative_words.append(line.strip())
-            
-    
 def inject(texts, percent):
     """ texts will be injected so that they then are at a injection rate of #percent
     """
@@ -109,12 +98,11 @@ drifted_embeddings.append(embed(drifted_texts))
 for num, perc in enumerate(target_percentages):
     drifted_texts = inject(drifted_texts, perc)
     drifted_embeddings.append(embed(drifted_texts))
-
     
 original_texts, original_keys = [list(t) for t in zip(*original_data)]
 original_embs = embed(original_texts)
 
 dump_data = {'orig': (original_embs, original_keys), 'drifted': (drifted_embeddings, drifted_keys)}
     
-with open('data/movies/embeddings/amazon_bert_small_gradual_drift_100.pickle', 'wb') as handle:
+with open(embeddings_file, 'wb') as handle:
     pickle.dump(dump_data, handle)
