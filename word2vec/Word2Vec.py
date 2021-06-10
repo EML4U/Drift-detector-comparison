9b9db2ceda68041e35e8a0d1e9fe519e21c144bf
Adrian Wilke <adibaba@users.noreply.github.com>
2021-06-10 21:27:10 +0200

    Word2Vec class

---
+++ word2vec/Word2Vec.py
@@ -0,0 +1,26 @@
+# Models are available at
+#  https://hobbitdata.informatik.uni-leipzig.de/EML4U/2021-05-17-Amazon-Doc2Vec/
+#
+# Minimal example:
+#  word2vec = Word2Vec("amazonreviews.model")
+#  word2vec.prepare()
+#  print(word2vec.embed(["Hello world", "Hey world"]))
+
+from gensim.models.doc2vec import Doc2Vec
+import gensim
+import numpy as np
+
+class Word2Vec():
+
+    def __init__(self, model_path):
+        self.model_path = model_path
+
+    def prepare(self):
+        self.model = Doc2Vec.load(self.model_path)
+
+    def embed(self, text_list):
+        embeddings = []
+        for text in text_list:
+            emb = self.model.infer_vector(gensim.utils.simple_preprocess(text))
+            embeddings.append(emb)
+        return np.stack(embeddings)
