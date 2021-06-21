10e1cd9766ec9b81790d115cb96d513c8f9f9fd7
Adrian Wilke <adibaba@users.noreply.github.com>
2021-06-21 21:46:51 +0200

    chi-squared categorical features test

--- detectors/AlibiChiSquaredDetector.py
+++ detectors/AlibiChiSquaredDetector.py
@@ -7,25 +7,35 @@ import numpy as np
 # https://en.wikipedia.org/wiki/Chi-squared_test
 class AlibiChiSquaredDetector(DriftDetector):
     
-    # p-value used for significance of the Chi-Squared test.
+    # p-value    used for significance of the Chi-Squared test.
     # correction Correction type for multivariate data. Either ‘bonferroni’ or ‘fdr’ (False Discovery Rate).
+    # multiplier e.g. 10, converts float values to create categories. Dev version, did not produce better results.
     def __init__(self,
                  p_val = 0.05,
-                 correction = "bonferroni"):
+                 correction = "bonferroni",
+                 multiplier = 1):
         self.p_val = p_val
         self.correction = correction
+        self.multiplier = multiplier
         super().__init__(classifier=None)
+    
+    # Dev version of preprocessing conversion, intended to generate categorical features
+    def create_categories(self, data):
+        if(self.multiplier != 1):
+            return np.round(np.multiply(data, self.multiplier))#.astype(int).astype(str)
+        else:
+            return data
         
     def fit(self, data, targets) -> DriftDetector:
         return self.fit(data)
     
     def fit(self, data) -> DriftDetector:
-        self.cd = ChiSquareDrift(data, p_val=self.p_val, correction=self.correction)
+        self.cd = ChiSquareDrift(self.create_categories(data), p_val=self.p_val, correction=self.correction)
         return self
     
     def predict(self, data) -> bool:
-        return self.cd.predict(data, drift_type='batch', return_p_val=False, return_distance=False).get('data').get('is_drift')
+        return self.cd.predict(self.create_categories(data), drift_type='batch', return_p_val=False, return_distance=False).get('data').get('is_drift')
     
     def predict_proba(self, data) -> float:
-        return np.mean(self.cd.predict(data, drift_type='batch', return_p_val=True, return_distance=False).get('data').get('p_val'))
+        return np.mean(self.cd.predict(self.create_categories(data), drift_type='batch', return_p_val=True, return_distance=False).get('data').get('p_val'))
 
