9f4ccda5999a042e16e214d20484f7d2c4a4f722
Adrian Wilke <adibaba@users.noreply.github.com>
2021-06-30 17:32:44 +0200

    results reader to reuse

--- evaluation/results_reader.py
+++ evaluation/results_reader.py
@@ -1,12 +1,13 @@
 # Usage example:
 # Import results reader functions
-#from results_reader import (read_results, print_overview)
-# Set directory with results files:
-#results_directory = "../data/results/"
+#from results_reader import (read_results, print_results, get_result_ids)
 # Read results:
-#results = read_results(results_directory, print_info=True)
-# Print overview of runtimes:
-#print_overview(results, ids=[], modes=[], detectors=[], keys=["time_detect", "time_fit"])
+#results = read_results("../data/results/", print_info=True)
+# Get sorted IDs of results:
+#ids, modes, detectors, keys = get_result_ids(results)
+#print(ids, modes, detectors, keys, sep="\n")
+# Print overview of runtimes for LSDD and BoW-50:
+#print_results(results, ids=[], modes=["bow_50"], detectors=["lsdd"], keys=["time_detect", "time_fit"])
 
 
 import collections
@@ -15,6 +16,7 @@ from os.path import isfile, join
 import pickle
 
 
+# Reads results files
 def read_results(results_directory, print_info=True):
     results = collections.OrderedDict()
 
@@ -42,25 +44,47 @@ def read_results(results_directory, print_info=True):
     if(print_info):
         for id_ in results: 
             print(
-                id_, " ",
-                results[id_]["result_pickle"], " ",
-                type(results[id_]["data"]), " ",
+                id_.ljust(25),
+                results[id_]["result_pickle"].ljust(50),
+                str(type(results[id_]["data"])).ljust(10),
                 len(results[id_]["data"])
             )
 
     return results
 
 
-def print_overview(results, ids=[], modes=[], detectors=[], keys=[]):
+# Prints overview of loaded results
+def print_results(results, ids=[], modes=[], detectors=[], keys=[]):
     for id_ in ids if ids else results:
         data = results[id_]["data"]
         for mode in modes if modes else data:
             for detector in detectors if detectors else data[mode]:
                 for key in keys if keys else data[mode][detector]:
-                    print(id_.ljust(23), mode.ljust(9), detector.ljust(5), key.ljust(12), end="")
-                    if key in data[mode][detector] and isinstance(data[mode][detector][key], list):
-                        print("", str(len(data[mode][detector][key])).ljust(4), end="")
+                    print(id_.ljust(23), mode.ljust(9), detector.ljust(5), key.ljust(13), end="")
+                    if key in data[mode][detector] and data[mode][detector][key]:
+                        print(str(type(data[mode][detector][key])).ljust(17), end="")
+                        if isinstance(data[mode][detector][key], list) or isinstance(data[mode][detector][key], dict):
+                            print(str(len(data[mode][detector][key])), "(len)")
+                        else:
+                            print(str(data[mode][detector][key])[:10])
                     else:
-                        print("     ", end="")
-                    print(results[id_]['result_pickle'], end="")
-                    print()
+                        print("None")
+
+
+# Gets lists of sorted IDs
+def get_result_ids(results, ids=[], modes=[], detectors=[], keys=[]):
+    ids_ = set()
+    modes_ = set()
+    detectors_ = set()
+    keys_ = set()
+    for id_ in ids if ids else results:
+        data = results[id_]["data"]
+        for mode in modes if modes else data:
+            for detector in detectors if detectors else data[mode]:
+                for key in keys if keys else data[mode][detector]:
+                    if key in data[mode][detector] and data[mode][detector][key]:
+                        ids_.add(id_)
+                        modes_.add(mode)
+                        detectors_.add(detector)
+                        keys_.add(key)
+    return sorted(ids_), sorted(modes_), sorted(detectors_), sorted(keys_)
