9abedd12eff6d0a0e7f25ed88e276f5378608b04
Adrian Wilke <adibaba@users.noreply.github.com>
2021-07-01 20:43:00 +0200

    rm dd

--- DriftDetectors/RegionalConceptDrift/getLDDbyKNN.m
+++
@@ -1,9 +0,0 @@
-function ldd = getLDDbyKNN(insts1, instsBase, knn)
-    
-    numBase = size(instsBase, 1);
-    nanb = knnsearch([instsBase; insts1], instsBase, 'K', knn);
-    na = sum(nanb<=numBase,2);
-    nb = sum(nanb>numBase,2);
-    ldd = nb./size(insts1, 1) ./ (na./size(instsBase, 1)) -1;
-    
-end
